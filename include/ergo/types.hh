323a2a370e013feccd159542c1e5f999c16b343b
Leonardo Mariscal <leo@ldmd.mx>
2021-12-07 17:50:11 +0000

    Add bool types for a correct memory representation

--- include/ergo/types.hh
+++ include/ergo/types.hh
@@ -9,12 +9,12 @@
 
 namespace ergo {
 
-  using i8  = std::int8_t;
+  using i8 = std::int8_t;
   using i16 = std::int16_t;
   using i32 = std::int32_t;
   using i64 = std::int64_t;
 
-  using u8  = std::uint8_t;
+  using u8 = std::uint8_t;
   using u16 = std::uint16_t;
   using u32 = std::uint32_t;
   using u64 = std::uint64_t;
@@ -23,6 +23,11 @@ namespace ergo {
   using f64 = double;
 
   using uchar = unsigned char;
+
+  using b8 = std::uint8_t;
+  using b16 = std::uint16_t;
+  using b32 = std::uint32_t;
+  using b64 = std::uint64_t;
 
 #ifndef M_PI
   constexpr f32 M_PI = 3.14159265358979323846;
--- test/test.cc
+++ test/test.cc
@@ -1,10 +1,30 @@
 #include <ergo/types.hh>
-
 #include <iostream>
 
 using namespace ergo;
 
 i32 main() {
   std::cout << "Hi there\n";
+
+  b8 x = true;
+  if (x) {
+    std::cout << "x is true\n";
+  } else {
+    std::cout << "x is false\n";
+  }
+
+  b32 y = true;
+  if (y) {
+    std::cout << "y is true\n";
+  } else {
+    std::cout << "y is false\n";
+  }
+
+  if (x == y) {
+    std::cout << "x == y\n";
+  } else {
+    std::cout << "x != y\n";
+  }
+
   return 0;
 }
